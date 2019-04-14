32d41299c87cb9f2c9e07154593ff57ec8b6bf43
Ali Akhtar <ali.akhter@vistajet.com>
2019-04-14 22:15:45 +0500

    Module Extension and Module Network framework added

---
+++ ModuleNetwork/ModuleNetwork/ModuleNetwork.h
@@ -0,0 +1,19 @@
+//
+//  ModuleNetwork.h
+//  ModuleNetwork
+//
+//  Created by Ali Akhtar on 14/04/2019.
+//  Copyright © 2019 Ali Akhtar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModuleNetwork.
+FOUNDATION_EXPORT double ModuleNetworkVersionNumber;
+
+//! Project version string for ModuleNetwork.
+FOUNDATION_EXPORT const unsigned char ModuleNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleNetwork/PublicHeader.h>
+
+
