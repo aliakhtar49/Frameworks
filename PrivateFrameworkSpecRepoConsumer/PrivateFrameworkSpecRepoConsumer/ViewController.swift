e4f353aafb9f6129a3d052c8ddd680b4a0cf5b06
Ali Akhtar <ali.akhter@vistajet.com>
2019-04-18 00:29:42 +0500

    Done with the podfile of consume private pods

---
+++ PrivateFrameworkSpecRepoConsumer/PrivateFrameworkSpecRepoConsumer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrivateFrameworkSpecRepoConsumer
+//
+//  Created by Ali Akhtar on 18/04/2019.
+//  Copyright © 2019 Ali Akhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
